72705e36cc17b31b02b05ade036650374342e432
Rajwansh Bhati <rajwansh.bhati@calsoftinc.com>
2025-06-23 12:17:33 +0530

    Last commit

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,8 +4,16 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: '0.0.0.0', // 👈 necessary for Docker access
-    port: 5173
+ server: {
+  host: '0.0.0.0',
+  port: 5173,
+  proxy: {
+    '/api': {
+      target: 'http://localhost:4000',
+      changeOrigin: true,
+      rewrite: path => path.replace(/^\/api/, '')
+    }
   }
+}
+
 });
