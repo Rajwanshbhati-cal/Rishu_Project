de22fbc7e90218fa0c0017674f918af3211b3221
Rajwansh Bhati <rajwansh.bhati@calsoftinc.com>
2025-06-24 14:25:30 +0530

    Last commit

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -5,14 +5,15 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0',
-    port: 5173,
     proxy: {
-      '/api': {
-        target: 'http://backend:4000',
+      '/get-names': {
+        target: 'http://localhost:4000',
         changeOrigin: true,
-        rewrite: path => path.replace(/^\/api/, '')
-      }
-    }
-  }
+      },
+      '/add-names': {
+        target: 'http://localhost:4000',
+        changeOrigin: true,
+      },
+    },
+  },
 });
